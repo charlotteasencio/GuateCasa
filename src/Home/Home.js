47b6e345efacd2ecc2d2032d3098153c0d486632
Charlotte Asencio <charlotteasencio@Charlottes-MacBook-Air.local>
2019-04-11 18:30:58 -0400

    add google maps

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './Home.scss' 
 import Search from '../Search'
+import Maps from '../Maps/Maps'
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div id='mainHomePage'>
-            <h1>This is the home page.</h1>
-            <h3>¿En donde quieres vivir?</h3>
-            <Search />
+            <div id="mainHomePage">
+                <div id='headerHomePage'>
+                    <h1>This is the home page.</h1>
+                    <h3>¿En donde quieres vivir?</h3>
+                    <Search />
+                </div>
+                <div id="mapsDiv">
+                    <Maps />
+                </div>
             </div>
         )
     }
