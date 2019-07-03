822e321933728e37b0c254da719566fd2d07c219
charlotteasencio <charlotte.e.asencio@gmail.com>
2019-07-03 13:43:00 -0400

    new commit

--- src/Maps/Maps.js
+++ src/Maps/Maps.js
@@ -10,7 +10,9 @@ import {
 const MyMapComponent = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBP05PesHZ69JFgRNp2y4AjQD-Y6AcSYO0&v=3.exp&libraries=geometry,drawing,places",
+      "https://maps.googleapis.com/maps/api/js?key=" +
+      process.env.REACT_APP_GOOGLE_API_KEY +
+      "&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: (
       <div style={{ height: `calc(100vh - 80px)`, width: `80%` }} />
