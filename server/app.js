533fceeb7d84dc449f23299f74eb65ad6382b7af
charlotteasencio <charlotte.e.asencio@gmail.com>
2019-07-10 16:52:48 -0400

    begin adding small mobile menu

--- server/app.js
+++ server/app.js
@@ -1,23 +1,28 @@
-const express = require ('express')
-const graphqlHTTP = require('express-graphql')
-const mongoose = require('mongoose')
-const schema = require('./schema/schema')
-require('dotenv').config()
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
+const schema = require("./schema/schema");
+require("dotenv").config();
 
-const app = express()
-const url = process.env.MONGOLAB_URI
+const app = express();
+//const url = process.env.MONGOLAB_URI;
 
-mongoose.connect(url)
-mongoose.connection.once('open', () => {
-    console.log('connected to database')
-})
+mongoose.connect(
+  "mongodb://charlotte:test123@ds347917.mlab.com:47917/guate-casa"
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     //this says really schema: schema but since it is the same word can just use schema
     schema,
     graphiql: true
-}))
+  })
+);
 
 app.listen(4000, () => {
-    console.log('listening for requests on port 4000')
-})
+  console.log("listening for requests on port 4000");
+});
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,9 +5,29 @@ import Comprar from "../Comprar";
 import Home from "../Home";
 import Alquiler from "../Alquiler";
 import Vender from "../Vender";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true,
+      isOpen: false
+    };
+  }
+
+  handleOpen = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+      isOpen: !this.state.isOpen
+    });
+  };
+
   render() {
+    const collapseState = this.state.collapsed ? "collapse" : "collapse show";
+
     return (
       <Router>
         <nav className="Navbar">
@@ -47,6 +67,81 @@ export default class Navbar extends React.Component {
               </Link>
             </li>
           </ul>
+        </nav>
+
+        <nav className="NavbarSmall">
+          <h4 className="Navbar__Logo">
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+              Guate Casa
+            </Link>
+          </h4>
+          <div className="navRight">
+            <div
+              id="menuButton"
+              onClick={this.handleOpen}
+              data-style="font-size: 140px"
+              toggle={collapseState}
+              data-target="#navLinks"
+              aria-expanded="true"
+              aria-controls="navLinks"
+            >
+              <div
+                id="menuButton"
+                onClick={this.handleOpen}
+                data-toggle={collapseState}
+                data-target="#navLinks"
+                aria-expanded="true"
+                aria-controls="navLinks"
+              >
+                {this.state.isOpen ? (
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    className="fa-lg NavbarSmall_MenuIcon"
+                  />
+                ) : (
+                  <FontAwesomeIcon
+                    icon={faBars}
+                    className="fa-lg NavbarSmall_MenuIcon"
+                  />
+                )}
+              </div>
+            </div>
+            <ul
+              id="navLinks"
+              className={collapseState}
+              aria-labelledby="menuButton"
+            >
+              <li className="Navbar__Link">
+                <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+                  Inicio
+                </Link>
+              </li>
+              <li className="Navbar__Link">
+                <Link
+                  to="/comprar"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Comprar
+                </Link>
+              </li>
+              <li className="Navbar__Link">
+                <Link
+                  to="/alquiler"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Alquiler
+                </Link>
+              </li>
+              <li className="Navbar__Link">
+                <Link
+                  to="/vender"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  Vender
+                </Link>
+              </li>
+            </ul>
+          </div>
         </nav>
 
         <Route exact path="/" component={Home} />
