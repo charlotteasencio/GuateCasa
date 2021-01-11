e87515677ebb417a7b9d621cac85255f38e75a02
Charlotte Asencio <charlotte.e.asencio@gmail.com>
2021-01-10 22:35:06 -0600

    Updates

---
+++ precache-manifest.fa2c4b689f1faac6d0d526ec630487b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9709c0ffc933793b4ff981ac5edc9f13",
+    "url": "/guateCasa/static/media/tealHomeIcon5.9709c0ff.png"
+  },
+  {
+    "revision": "fe33b66d216223430386164e2e698f9e",
+    "url": "/guateCasa/static/media/tealHomeIcon4.fe33b66d.png"
+  },
+  {
+    "revision": "a49ad4418bf69b42f181bd0a3dfa720b",
+    "url": "/guateCasa/static/media/tealHomeIcon3.a49ad441.png"
+  },
+  {
+    "revision": "ca73030f369f43b98f42",
+    "url": "/guateCasa/static/js/runtime~main.346e9c91.js"
+  },
+  {
+    "revision": "02a9ced4fa9adca778dc",
+    "url": "/guateCasa/static/js/main.2669ab92.chunk.js"
+  },
+  {
+    "revision": "1f86e370d62139efc07b",
+    "url": "/guateCasa/static/js/2.81447120.chunk.js"
+  },
+  {
+    "revision": "02a9ced4fa9adca778dc",
+    "url": "/guateCasa/static/css/main.76bb9a4f.chunk.css"
+  },
+  {
+    "revision": "fa3e6f03eeceabd156ba8700fd7a0d81",
+    "url": "/guateCasa/index.html"
+  }
+];
